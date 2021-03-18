b07fb9d5e89d0905b9297d353010477525965994
asitball <asit3310@gmail.com>
2021-03-18 22:37:36 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import { useEffect, useState } from 'react';
 import './App.css';
+import Cart from './components/Cart/Cart';
 import Player from './components/Player/Player';
 import playersData from './data/data.json';
 
 function App() {
-  const [player, setPlayer] = useState([])
+  const [player, setPlayer] = useState([]);
+  const [cart, setCart] = useState([]);
   useEffect(()=> {
     setPlayer(playersData);
   },[])
+  const handleAddPlayer = (player)=> {
+    const newCart = [...cart, player];
+     setCart(newCart);
+  }
   return (
-    <div>
-      {
-        playersData.map(player=> <Player player={player}></Player>)
-      }
+    <div className="container">
+       <div className="row">
+            <div className='col-md-8 player-container'>
+            {
+              playersData.map(player=> <Player player= {player} handleAddPlayer={handleAddPlayer}></Player>)
+            }
+          </div>
+          <div className="col-md-4 count-container">
+            <h3>Total Players: {player.length}</h3>
+            <h4>Player Added:  {cart.length}</h4>
+            <Cart cart={cart}></Cart>
+          </div>
+       </div>
     </div>
   );
 }
--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import './Player.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons'
 
 const Player = (props) => {
-    const {name, image, salary} = props.player
+    const {name, image, salary} = props.player;
+    const handleAddPlayer = props.handleAddPlayer;
         return (
-        <div>
+        <div  className="players-info">
             <h2>{name}</h2>
             <img src={image} alt=""/>
-            <h5>{salary}</h5>
+            <h5> Salary: {salary}</h5>
+            <button className="btn btn-primary" onClick={()=>handleAddPlayer(props.player)} >Player Add <FontAwesomeIcon icon={faPlus} /></button>
+           
         </div>
     );
 };
