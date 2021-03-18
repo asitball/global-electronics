3065810de3f52942736e701aa5ab8da4f1dbab02
asitball <asit3310@gmail.com>
2021-03-18 17:18:25 +0600

    code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import playersData './data/data.json';
 
 function App() {
+  const [player, setPlayer] = useState([])
+  useEffect(()=> {
+    setPlayer(playersData);
+    console.log(playersData);
+  },[])
   return (
     <div>
       
