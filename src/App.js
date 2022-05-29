d0eeb3a542cdb1276bb1e95416854fce8ba76418
asitball <asit3310@gmail.com>
2022-05-29 22:13:11 +0600

    project connection with github

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Cart from './components/Cart/Cart';
-import Player from './components/Player/Player';
-import playersData from './data/data.json';
+import React from 'react';
+import Header from './component/Header/Header';
+import Home from './component/Home/Home';
 
-function App() {
-  const [player, setPlayer] = useState([]);
-  const [cart, setCart] = useState([]);
-  useEffect(()=> {
-    setPlayer(playersData);
-  },[])
-  const handleAddPlayer = (player)=> {
-    const newCart = [...cart, player];
-     setCart(newCart);
-  }
+const App = () => {
   return (
-    <div className="container">
-       <div className="row">
-            <div className='col-md-8 player-container'>
-            {
-              playersData.map(player=> <Player player= {player} handleAddPlayer={handleAddPlayer}></Player>)
-            }
-          </div>
-          <div className="col-md-4 count-container">
-            <h3>Total Players: {player.length}</h3>
-            <h4>Player Added:  {cart.length}</h4>
-            <Cart cart={cart}></Cart>
-          </div>
-       </div>
+    <div>
+      <Header></Header>
+        <Home></Home>
     </div>
   );
-}
+};
 
 export default App;
