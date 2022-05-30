aa1d94cb0f3af044cd70470a2351a4ae175a7fbd
asitball <asit3310@gmail.com>
2022-05-30 21:28:34 +0600

    review section added

--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -1,18 +1,30 @@
 import React from 'react';
 import './Home.css';
 import image from '../../images/fridge.jpg';
+import ReviewData from '../../hooks/ReviewData';
+import Reviews from '../Reviews/Reviews';
 const Home = () => {
+    const [reviews, setReviews] = ReviewData();
+
     return (
-        <div className='product-into-container'>
-            <div className="product-into">
-                <h1>Keeps your food <br /> <span className='highlight-title'>Fresher Longer</span></h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui hic eum cupiditate consequuntur iusto beatae perspiciatis rerum ut ab nihil?</p>
-                <button className='live-demo-btn'>Live demo</button>
-            </div>
-            <div className="product-image">
-                <img src={image} alt="" />
+        <div>
+            <div className='product-into-container'>
+                <div className="product-into">
+                    <h1>Keeps your food <br /> <span className='highlight-title'>Fresher Longer</span></h1>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui hic eum cupiditate consequuntur iusto beatae perspiciatis rerum ut ab nihil?</p>
+                    <button className='live-demo-btn'>Live demo</button>
+                </div>
+                <div className="product-image">
+                    <img src={image} alt="" />
+                </div>
+           </div>
+            <div className='review-container'>
+                {
+                    reviews.map(review => <Reviews review={review}></Reviews>)
+                }
             </div>
         </div>
+
     );
 };
 
--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import './Reviews.css'
-
-const Reviews = () => {
-  
+const Reviews = (props) => {
+    const { name, comment, rating, image } = props.review;
     return (
-        <div>
-
+        <div className='review-container'>
+                <img src={image} alt="" />
+            <div>
+                <h3>{name}</h3>
+                <h4>{comment}</h4>
+                <p>{rating}</p>
+            </div>
         </div>
     );
 };
