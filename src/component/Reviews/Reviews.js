039efe054adaf0d234ec405acd863de6b4d4ae21
asitball <asit3310@gmail.com>
2022-05-30 21:51:01 +0600

    review improve

--- src/component/Reviews/Reviews.js
+++ src/component/Reviews/Reviews.js
@@ -3,12 +3,12 @@ import './Reviews.css'
 const Reviews = (props) => {
     const { name, comment, rating, image } = props.review;
     return (
-        <div className='review-container'>
-                <img src={image} alt="" />
+        <div className='reviews-container'>
+            <img src={image} alt="" />
             <div>
-                <h3>{name}</h3>
-                <h4>{comment}</h4>
-                <p>{rating}</p>
+            <h2>{name}</h2>
+            <p>{comment}</p>
+            <p>{rating}</p>
             </div>
         </div>
     );
